ac286274245df45bd3257477db4b649193406260
dearmaster windows7 <694114533@qq.com>
2016-10-22 14:18:28 +0800

    message producer and consumer fine

---
+++ JMS/JMS-simple/src/main/java/com/master/jms/Receiver.java
@@ -0,0 +1,59 @@
+package com.master.jms;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.log4j.Logger;
+
+import javax.jms.*;
+
+public class Receiver {
+
+    private static final Logger logger = Logger.getLogger(Receiver.class);
+
+    private static final String queueName = "TEST_QUEUE";
+    private static final String brokerUrl = "tcp://master:61616";
+
+    public static void main(String[] args) {
+        new Receiver().receive();
+    }
+
+    private void receive() {
+        ConnectionFactory factory;
+        Connection connection = null;
+        Session session;
+        Destination destination;
+        MessageConsumer consumer;
+
+        factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, brokerUrl);
+
+        try {
+            connection = factory.createConnection();
+            connection.start();
+
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            destination = session.createQueue(queueName);
+            consumer = session.createConsumer(destination);
+
+            while (true) {
+                TextMessage msg = (TextMessage) consumer.receive(10000);
+                if(null != msg) {
+                    logger.debug("收到消息：" + msg.getText());
+                } else {
+                    logger.debug("没有收到消息");
+                    break;
+                }
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
+            if(null != connection) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ JMS/JMS-simple/src/main/java/com/master/jms/Sender.java
@@ -0,0 +1,60 @@
+package com.master.jms;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.log4j.Logger;
+
+import javax.jms.*;
+
+public class Sender {
+
+    private static final Logger logger = Logger.getLogger(Sender.class);
+
+    private static final String queueName = "TEST_QUEUE";
+    private static final String brokerUrl = "tcp://master:61616";
+
+    public static void main(String[] args) {
+        new Sender().send("Hello World");
+    }
+
+    private void send(String msg) {
+        ConnectionFactory factory;
+        Connection connection = null;
+        Session session;
+        Destination destination;
+        MessageProducer producer;
+
+        factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, brokerUrl);
+
+        try {
+            connection = factory.createConnection();
+            connection.start();
+
+            session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            destination = session.createQueue(queueName);
+
+            producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            sendMessage(session, producer, msg);
+            session.commit();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != connection) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void sendMessage(Session session, MessageProducer producer, String msg) throws JMSException {
+        TextMessage textMsg = session.createTextMessage(msg);
+        logger.debug("发送消息：" + msg);
+        producer.send(textMsg);
+    }
+
+}
